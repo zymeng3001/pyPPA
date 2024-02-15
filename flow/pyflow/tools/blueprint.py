536db5e9782009665734e1374673f2c7c61e908c
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-02-15 22:38:49 +0530

    feat: added APRTool blueprint

--- flow/pyflow/tools/blueprint.py
+++ flow/pyflow/tools/blueprint.py
@@ -16,10 +16,12 @@ class FlowTool:
 	tool_cmd: str
 	tool_default_args: list[str]
 	scripts_dir: str
+	log_dir: str
 
-	def __init__(self, cmd: str, scripts_dir: str, default_args: list[str] = []):
+	def __init__(self, cmd: str, scripts_dir: str, log_dir: str, default_args: list[str] = []):
 		self.tool_cmd = cmd
 		self.scripts_dir = scripts_dir
+		self.log_dir = log_dir
 		self.tool_default_args = default_args
 
 	def _call_tool(self, args: list[str], env: dict | None, logfile: str | None):
@@ -42,10 +44,54 @@ class SynthStats(TypedDict):
 	module_area: float
 
 class SynthTool(FlowTool):
-	def run_synth(self, env: dict[str, str], logfile: str):
+	def run_synth(self, env: dict[str, str]):
 		"""Runs the synthesis script."""
 		pass
 
 	def parse_synth_stats(self, raw_stats: str) -> SynthStats:
 		"""Parses generated synthesis stats."""
 		pass
+
+class STAReport(TypedDict):
+	clk_name: str
+	clk_period: float
+	clk_slack: float
+
+class FloorplanningStats(TypedDict):
+	num_sequential_cells: int
+	num_combinational_cells: int
+	sta: dict[str, STAReport]
+
+class PowerReportEntry(TypedDict):
+	internal_power: str
+	switching_power: str
+	leakage_power: str
+	total_power: str
+	percentage: float
+
+class PowerReportTotalPercentages(TypedDict):
+	internal_power: float
+	switching_power: float
+	leakage_power: float
+
+class PowerReport(TypedDict):
+	sequential: PowerReportEntry
+	combinational: PowerReportEntry
+	clock: PowerReportEntry
+	macro: PowerReportEntry
+	pad: PowerReportEntry
+
+	total: PowerReportEntry
+	total_percentages: PowerReportTotalPercentages
+
+class APRTool(FlowTool):
+	def run_floorplanning(self, env: dict[str, str], logfile: str):
+		"""Runs the floorplanning script."""
+		pass
+
+	def parse_floorplanning_stats(self, raw_stats: str) -> FloorplanningStats:
+		"""Parses general floorplanning stats."""
+		pass
+
+	def parse_power_report(self, raw_report: str) -> PowerReport:
+		"""Parses the power report"""
