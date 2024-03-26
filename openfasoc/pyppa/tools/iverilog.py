8f312a6fea39005e597bcfdaa74c33147a87d7ad
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-03-26 15:47:46 +0530

    - fix: renamed class to `Iverilog` - feat: added sv support in default arguments - refactor: renamed `top_module` to `testbench_mdoule` - fix: create the objects dir if it doesnt exist - fix: use absolute paths for the verilog files and testbench file

--- openfasoc/pyppa/tools/iverilog.py
+++ openfasoc/pyppa/tools/iverilog.py
@@ -1,12 +1,12 @@
-from os import path
+from os import path, mkdir
 import shutil
 from .blueprint import VerilogSimTool, call_cmd
 
-class Verilator(VerilogSimTool):
+class Iverilog(VerilogSimTool):
 	vvp_cmd: str
 	defautl_vvp_args: list[str]
 
-	def __init__(self, scripts_dir: str, default_args: list[str] = [], cmd: str = "iverilog", default_vvp_args: list[str] = [], vvp_command: str = "vvp"):
+	def __init__(self, scripts_dir: str, default_args: list[str] = ["-g2005-sv"], cmd: str = "iverilog", default_vvp_args: list[str] = [], vvp_command: str = "vvp"):
 		super().__init__(scripts_dir, default_args, cmd)
 		self.defautl_vvp_args = default_vvp_args
 		self.vvp_cmd = shutil.which(vvp_command)
@@ -14,7 +14,7 @@ class Verilator(VerilogSimTool):
 	def run_sim(
 		self,
 		verilog_files: list[str],
-		top_module: str,
+		testbench_module: str,
 		testbench_file: str,
 		obj_dir: str,
 		vcd_file: str,
@@ -22,10 +22,17 @@ class Verilator(VerilogSimTool):
 		env: dict[str, str]
 	):
 		objects_dir = path.join(obj_dir, 'iverilog')
+		if not path.exists(objects_dir):
+			mkdir(objects_dir)
 
 		# Compile the testbench
 		self._call_tool(
-			args=['-o', top_module, '-s', top_module, testbench_file,  *verilog_files],
+			args=[
+				'-o', testbench_module,
+				'-s', testbench_module,
+				path.abspath(testbench_file),
+				*[path.abspath(file) for file in verilog_files]
+			],
 			env=env,
 			logfile=path.join(log_dir, '0_1_1_iverilog_compile.log'),
 			cwd=objects_dir
@@ -34,7 +41,7 @@ class Verilator(VerilogSimTool):
 		# Run the testbench
 		call_cmd(
 			cmd=self.vvp_cmd,
-			args=self.defautl_vvp_args + [top_module],
+			args=self.defautl_vvp_args + [testbench_module],
 			env=env,
 			logfile=path.join(log_dir, '0_1_2_iverilog_run.log'),
 			cwd=objects_dir
