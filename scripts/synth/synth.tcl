565c0160a466d8f161a755361bc46c9f208cdd97
Evansika959 <evansjiang0959@gmail.com>
2024-12-30 14:56:56 -0500

    update script

--- scripts/synth/synth.tcl
+++ scripts/synth/synth.tcl
@@ -132,7 +132,7 @@ if {$buffering==1} {
     set abc_fine_tune       ""
 }
 
-set abc_script "read design.blif;fx;mfs;strash;${abc_rf};${abc_resyn2};${abc_retime_dly}; scleanup;${abc_map_old_dly};retime,-D,{D};&get,-n;&st;&dch;&nf;&put;${abc_fine_tune};stime,-p;print_stats -m;write output.blif"
+# set abc_script "read design.blif;fx;mfs;strash;${abc_rf};${abc_resyn2};${abc_retime_dly}; scleanup;${abc_map_old_dly};retime,-D,{D};&get,-n;&st;&dch;&nf;&put;${abc_fine_tune};stime,-p;print_stats -m;write output.blif"
 
 set constr1 [open $::env(OBJECTS_DIR)/abc.script w]
 puts $constr1 "fx;mfs;strash;${abc_rf};${abc_resyn2};${abc_retime_dly}; scleanup;${abc_map_old_dly};retime,-D,{D};&get,-n;&st;&dch;&nf;&put;${abc_fine_tune};stime,-p;print_stats -m"
@@ -143,7 +143,7 @@ close $constr1
 if {[info exist ::env(ABC_CLOCK_PERIOD_IN_PS)]} {
   puts "\[FLOW\] Set ABC_CLOCK_PERIOD_IN_PS to: $::env(ABC_CLOCK_PERIOD_IN_PS)"
   abc -D [expr $::env(ABC_CLOCK_PERIOD_IN_PS)] \
-      -script "$abc_script" \
+      -script $abc_script \
       -liberty $::env(DONT_USE_SC_LIB) \
       -constr $::env(OBJECTS_DIR)/abc.constr
 } else {
