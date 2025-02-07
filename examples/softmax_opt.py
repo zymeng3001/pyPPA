8f52cc5a937fe8bfb5cfa9c27fa389601abd6fae
Evansika959 <evansjiang0959@gmail.com>
2025-02-07 00:53:01 -0500

    update:

--- examples/softmax_opt.py
+++ examples/softmax_opt.py
@@ -51,7 +51,7 @@ study_config.algorithm = 'DEFAULT'
 study_client = clients.Study.from_study_config(
   study_config,
   owner='ppa_runner',
-  study_id='ppa_softmax_optimizer'
+  study_id='ppa_softmax_optimizer_v2'
 )
 print('Local SQL database file located at: ', service.VIZIER_DB_PATH)
 
