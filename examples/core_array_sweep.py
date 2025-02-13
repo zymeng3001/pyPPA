0bdc8a8cf3065e49767c98d7ea3676e31fd7308b
Evansika959 <evansjiang0959@gmail.com>
2025-02-13 16:49:11 -0500

    update core array sweep

--- examples/core_array_sweep.py
+++ examples/core_array_sweep.py
@@ -40,7 +40,7 @@ problem.search_space.root.add_float_param(name='constraint_period', min_value=8,
 # problem.search_space.root.add_int_param(name='ABC_MAX_FANOUT', min_value=12, max_value=28, default_value=20) # Guessing the ABC max fanout is somewhere between 12 and 28
 # problem.search_space.root.add_float_param(name='ABC_MAP_EFFORT', min_value=0, max_value=1, default_value=0.6) # Guessing the ABC map effort is somewhere between 0 and 1
 problem.search_space.root.add_int_param(name='n_heads', min_value=1, max_value=12, default_value=4) 
-problem.search_space.root.add_int_param(name='n_cols', min_value=1, max_value=16, default_value=4) 
+problem.search_space.root.add_int_param(name='n_cols', min_value=2, max_value=16, default_value=4) 
 problem.search_space.root.add_discrete_param(name='head_dim', feasible_values=np.arange(32,288,32).tolist(), default_value=64) 
 problem.search_space.root.add_discrete_param(name='max_context_length', feasible_values=np.arange(8,264,8).tolist(), default_value=64)
 problem.search_space.root.add_discrete_param(name='gbus_width', feasible_values=np.arange(8,264,8).tolist(), default_value=64)
@@ -53,7 +53,7 @@ problem.metric_information.append(
 )
 
 study_config = vz.StudyConfig.from_problem(problem)
-study_config.algorithm = 'NSGA2' # Use NSGA2 for multi-objective optimization
+study_config.algorithm = 'DEFAULT' # Use NSGA2 for multi-objective optimization
 study_client = clients.Study.from_study_config(
   study_config,
   owner='ppa_runner',
