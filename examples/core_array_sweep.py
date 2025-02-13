ddf67429d465d3c179a4e03f6d257ecbfbdf0d7d
Evansika959 <evansjiang0959@gmail.com>
2025-02-13 16:39:11 -0500

    update core array sweep

--- examples/core_array_sweep.py
+++ examples/core_array_sweep.py
@@ -57,7 +57,7 @@ study_config.algorithm = 'NSGA2' # Use NSGA2 for multi-objective optimization
 study_client = clients.Study.from_study_config(
   study_config,
   owner='ppa_runner',
-  study_id='ppa_core_array_opt_v1'
+  study_id='ppa_core_array_opt_v2'
 )
 print('Local SQL database file located at: ', service.VIZIER_DB_PATH)
 
