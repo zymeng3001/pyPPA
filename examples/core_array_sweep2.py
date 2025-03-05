5be7cde048bc2a06262a444c93c0c794154d6b4a
Evansika959 <evansjiang0959@gmail.com>
2025-03-05 01:52:18 -0700

    update:

--- examples/core_array_sweep2.py
+++ examples/core_array_sweep2.py
@@ -43,7 +43,7 @@ problem.search_space.root.add_int_param(name='constraint_period', min_value=5, m
 problem.search_space.root.add_int_param(name='n_heads', min_value=1, max_value=12, default_value=4) 
 problem.search_space.root.add_int_param(name='n_cols', min_value=1, max_value=16, default_value=10) 
 problem.search_space.root.add_discrete_param(name='head_dim', feasible_values=[256], default_value=256) 
-problem.search_space.root.add_discrete_param(name='max_context_length', feasible_values=np.arange(8,136,8).tolist(), default_value=32)
+problem.search_space.root.add_discrete_param(name='max_context_length', feasible_values=np.arange(8,72,8).tolist(), default_value=32)
 problem.search_space.root.add_discrete_param(name='gbus_width', feasible_values=[16,24,32], default_value=16)
 
 problem.metric_information.append(
@@ -58,7 +58,7 @@ study_config.algorithm = 'RANDOM_SEARCH' # Use random search for random sampling
 study_client = clients.Study.from_study_config(
   study_config,
   owner='ppa_runner',
-  study_id='ppa_core_array_opt_mar4'
+  study_id='ppa_core_array_opt_mar5'
 )
 print('Local SQL database file located at: ', service.VIZIER_DB_PATH)
 
@@ -86,7 +86,7 @@ def is_feasible(suggestion) -> bool:
 	# 	print(f"max_context_length {max_context_length} is not divisible by n_cols {n_cols}. Reject suggestion.")
 	# 	return False
 
-	if n_heads > 7 and n_cols*n_heads > 63:
+	if n_heads > 7 or n_cols*n_heads > 80:
 		print(f"n_heads * n_cols {n_heads * n_cols} is greater than 64. Reject suggestion")
 		return False
 
