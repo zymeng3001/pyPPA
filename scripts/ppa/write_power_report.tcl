cd25f4f890a0803fc5d98bd72c673df30e6b5faf
Evansika959 <evansjiang0959@gmail.com>
2025-03-15 22:43:06 -0400

    fix power report

--- scripts/ppa/write_power_report.tcl
+++ scripts/ppa/write_power_report.tcl
@@ -15,8 +15,30 @@ proc write_power_report {step} {
 			sta::set_power_activity -input_port rst -activity 0
 		}
 
+		current_design $::env(DESIGN_NAME)
+
 		set power_report_file [file join $::env(REPORTS_DIR) "${step}_power_report.txt"]
 		sta::report_power > $power_report_file
+
+		set fp [open $power_report_file w]
+
+        # Initialize total power
+        set total_power 0.0
+
+        # Get power for each module
+        foreach module [get_designs] {
+            current_design $module
+            set power [sta::report_power -quiet]  ;# Get power of the module
+            puts $fp "$module: $power mW"
+            set total_power [expr $total_power + $power]
+        }
+
+        # Print total power at the end of the report
+        puts $fp "------------------------------------"
+        puts $fp "Total Power (Summed from Modules): $total_power mW"
+        puts $fp "------------------------------------"
+
+        close $fp
 
 		unset_propagated_clock [all_clocks]
 	}
