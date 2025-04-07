2251c13286436d3f7848d1e61c86fa623aa10e93
Evansika959 <evansjiang0959@gmail.com>
2025-04-07 14:49:52 -0400

    update

--- arch_explore/core_top_sweep.py
+++ arch_explore/core_top_sweep.py
@@ -54,8 +54,8 @@ problem = vz.ProblemStatement()
 problem.search_space.root.add_discrete_param(name='constraint_period', feasible_values=[6], default_value=6) # Guessing that the optimal period is somewhere in between, based on previous results
 # problem.search_space.root.add_int_param(name='ABC_MAX_FANOUT', min_value=12, max_value=28, default_value=20) # Guessing the ABC max fanout is somewhere between 12 and 28
 # problem.search_space.root.add_float_param(name='ABC_MAP_EFFORT', min_value=0, max_value=1, default_value=0.6) # Guessing the ABC map effort is somewhere between 0 and 1
-problem.search_space.root.add_discrete_param(name='wmem_depth', feasible_values=[32, 64, 128] + np.arange(256, 4352, 256).tolist(), default_value=256)
-problem.search_space.root.add_discrete_param(name='cache_depth', feasible_values=[32, 64, 128] + np.arange(256, 4352, 256).tolist(), default_value=256)
+problem.search_space.root.add_discrete_param(name='wmem_depth', feasible_values=[32, 64, 128, 256, 512, 1024, 1536, 2048], default_value=256)
+problem.search_space.root.add_discrete_param(name='cache_depth', feasible_values=[32, 64, 128, 256, 512, 1024, 1536, 2048], default_value=256)
 problem.search_space.root.add_discrete_param(name='gbus_width', feasible_values=[16,32,64,128], default_value=32)
 
 problem.metric_information.append(
