c3fb8cd4b4c8bac0ab85e4834cc6abf5ffbef6ef
Evansika959 <evansjiang0959@gmail.com>
2025-04-13 00:53:51 -0400

    update

--- arch_explore/activation_sweep.py
+++ arch_explore/activation_sweep.py
@@ -33,7 +33,7 @@ ppa_runner = PPARunner(
 			path.join(path.dirname(__file__), 'HW', 'vector_engine/activation/relu.v'),
 			path.join(path.dirname(__file__), 'HW', 'vector_engine/activation/silu.v'),
 			path.join(path.dirname(__file__), 'HW', 'vector_engine/activation/gelu.v'),
-			path.join(path.dirname(__file__), 'HW', 'vector_engine/activation/softmax.v')
+			path.join(path.dirname(__file__), 'HW', 'vector_engine/activation/softplus.v')
 		],
 		'SDC_FILE': path.join(path.dirname(__file__), 'HW', 'constraint.sdc')
 	}
@@ -55,7 +55,7 @@ study_config.algorithm = 'RANDOM_SEARCH'
 study_client = clients.Study.from_study_config(
   study_config,
   owner='ppa_runner',
-  study_id='ppa_activation_sweep_v1'
+  study_id='ppa_activation_sweep_v2'
 )
 print('Local SQL database file located at: ', service.VIZIER_DB_PATH)
 
