5883ca4eb4c4e8280cd646dfba2f2b61e31372b0
Evansika959 <evansjiang0959@gmail.com>
2025-04-21 17:34:32 -0400

    update

--- arch_explore/core_array_sweep/plot_sw_sweep.py
+++ arch_explore/core_array_sweep/plot_sw_sweep.py
@@ -16,7 +16,7 @@ print(f"Number of datapoints with Val_Loss < 5.0: {num_datapoints}")
 plt.figure(figsize=(10, 6))
 scatter = plt.scatter(
     filtered_df["Token Delay(ms)"],
-    filtered_df["Energy per Token(mJ)"],
+    filtered_df["Energy per Token(uJ)"],
     c=filtered_df["val_loss"],  # Assuming val_loss is used as a proxy for perplexity
     cmap="plasma",
     alpha=0.85,
@@ -25,24 +25,24 @@ scatter = plt.scatter(
 
 # Add colorbar for perplexity
 cbar = plt.colorbar(scatter)
-cbar.set_label("Perplexity (Validation Loss)")
+cbar.set_label("Validation Loss")
 
 # Axis labels and title
-plt.ylabel("Energy per Token (mJ)")
+plt.ylabel("Energy per Token (uJ)")
 plt.xlabel("Token Delay (ms)")
 plt.title("Energy vs. Token Delay (Colored by Perplexity)")
 plt.grid(True)
 plt.tight_layout()
 
 # Save the plot
-plt.savefig("Energy_vs_TokenDelay_by_Perplexity.png", dpi=300)
+plt.savefig("plots/Energy_vs_TokenDelay_by_Perplexity.png", dpi=300)
 
 ############################################
 
 # Create scatter plot: Energy vs. Token Delay, colored by Perplexity
 plt.figure(figsize=(10, 6))
 scatter = plt.scatter(
-    filtered_df["Energy per Token(mJ)"],
+    filtered_df["Energy per Token(uJ)"],
     filtered_df["val_loss"],
     c=filtered_df["n_embed"],  # Assuming val_loss is used as a proxy for perplexity
     cmap="plasma",
@@ -55,8 +55,8 @@ cbar = plt.colorbar(scatter)
 cbar.set_label("n_embed")
 
 # Axis labels and title
-plt.ylabel("Energy per Token (mJ)")
-plt.xlabel("Token Delay (ms)")
+plt.ylabel("Validation Loss")
+plt.xlabel("Energy per Token (uJ)")
 # plt.title("Energy vs. Token Delay")
 plt.grid(True)
 plt.tight_layout()
